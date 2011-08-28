fc03114430f521f88f0660b2c12a8380d9a38aa1
root <root@vagrantup.com>
2011-08-28 13:45:03 -0700

    Added last.fm logic to birds_processor. Just need to figure out how to remove DJ talking

--- birds_processor.py
+++ birds_processor.py
@@ -2,20 +2,26 @@ from BeautifulSoup import BeautifulSoup
 import requests
 import datetime
 import os
+import time
 from subprocess import call
 from operator import itemgetter
 from hsaudiotag import auto
+import pylast
 # Use time offset info from url to chop up mp3 and then tag it with info from the table
 pl_url = 'http://www.wnur.org/playlist/playlists/?dj=Joe+G'
+last_fm_api_key = 'da2f492ee9d414260f8ff9e252ab00f3'
+last_fm_api_secret = 'cf877f343b4465d8ac3fc786818c299b'
+podcasts_dir = '../podcasts/'
 
 r = requests.get(pl_url)
-soup = BeautifulSoup(r.content)
+soup = BeautifulSoup(r.content, convertEntities=BeautifulSoup.HTML_ENTITIES)
 podcasts = {}
-for file in os.walk('podcasts/'):
+for file in os.walk(podcasts_dir):
     for f in file[2]:
         podcasts[datetime.datetime.strptime(f[-14:].rstrip('mp3').replace('.',''), '%Y-%m-%d').strftime('%m/%d/%Y')] = f
 h2s = soup.findAll('h2')
 for k,v in podcasts.items():
+    print 'Processing %s' % v
     for h2 in h2s:
         if h2.next.next.text == k:
             table = h2.nextSibling
@@ -31,16 +37,24 @@ for k,v in podcasts.items():
             pl = sorted(pl, key=itemgetter(0))
             starttime = pl[0][0]
             dirname = '-'.join(k.split('/'))
-            call(['mkdir', dirname])
-            for index,tr in enumerate(pl):
-                tr_start = tr[0] - starttime
-                tr_start = str(round(float(tr_start.seconds)/60,2))
+            call(['mkdir', podcasts_dir + dirname])
+            tr_start = 0.0
+            client = pylast.LastFMNetwork(api_key = last_fm_api_key, api_secret = last_fm_api_secret)
+            file_duration =  str(round(int(auto.File(podcasts_dir + v).duration)/60,2))
+            for tr in pl:
                 try:
-                    tr_end = pl[index+1][0] - starttime
-                    tr_end = str(round(float(tr_end.seconds)/60,2))
-                except IndexError:
-                    tr_end = str(round(int(auto.File('podcasts/' + v).duration)/60,2))
-                print 'tr_start: %s, tr_end: %s' % (tr_start, tr_end)
+                    print 'Looking up: %s - %s' % (tr[1],tr[2])
+                    track = client.get_track(tr[1],tr[2])
+                    tr_duration = float(time.strftime('%M.%S', time.gmtime(track.get_duration()/1000)))
+                    if tr_duration == '0.0':
+                        print 'No duration listed in last.fm'
+                        continue
+                    else:
+                        print 'Found it. It\'s %s minutes long' % str(tr_duration)
+                        tr_end = tr_start + tr_duration
+                        call(['mp3splt', podcasts_dir + v, str(tr_start), str(tr_end), '-d ' + dirname, '-g ' + '[@a=' + '"' + tr[1] + '"' + ',@b=' + '"' + tr[3] + '"' + ',@t=' + '"' + tr[2] + '"' + ']'])
+                        tr_start = tr_end
+                except pylast.WSError, e:
+                    print 'Last.fm says: %s ' % e
 #                use this https://github.com/discogs/discogs-python-client
 #                call('mp3splt' + ' podcasts/' + v + ' ' + tr_start + ' ' + tr_end + ' -o ' + '_'.join(tr[2].split(' ')) + '.mp3')
-                call(['mp3splt', 'podcasts/' + v, tr_start, tr_end, '-d ' + dirname, '-g ' + '[@a=' + tr[1] + ',@b=' + tr[3] + ',@t=' + tr[2] + ']'])
--- notes.py
+++ notes.py
@@ -1,12 +1,8 @@
 from BeautifulSoup import BeautifulSoup
 import requests
-import json
-import urllib
-import math
 import pylast
 
 pl_url = 'http://www.wnur.org/playlist/playlists/?dj=Joe+G'
-lastfm_url = 'http://ws.audioscrobbler.com/2.0/'
 last_fm_api_key = 'da2f492ee9d414260f8ff9e252ab00f3'
 last_fm_api_secret = 'cf877f343b4465d8ac3fc786818c299b'
 r = requests.get(pl_url)
@@ -23,9 +19,10 @@ for row in rows[1:]:
 
 client = pylast.LastFMNetwork(api_key = last_fm_api_key, api_secret = last_fm_api_secret)
 for tr in pl:
-    print 'Query string: %s' % tr[3]
-    search = client.search_for_album(tr[3])
-    for res in search.get_next_page():
-        for track in res.get_tracks():
-            if tr[2] in track.get_title() and tr[1] in track.get_artist().get_name():
-                print 'Artist: %s, Title: %s, Duration: %s' % (track.get_artist(), track.get_title(), track.get_duration())
+    print 'Query: %s - %s' % (tr[1],tr[2])
+    try:
+        track = client.get_track(tr[1],tr[2])
+        print 'Duration: %s' % str(float(track.get_duration())/1000)
+    except pylast.WSError, e:
+        print e
+
